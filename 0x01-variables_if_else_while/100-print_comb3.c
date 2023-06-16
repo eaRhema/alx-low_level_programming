80e260f3cf964735e1e6eb9be94833d9b8adce66
emmanuel addy kofi okrah <emmanueladdy16.ea@gmail.com>
2023-06-16 10:57:28 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,14 +7,12 @@
 int main(void)
 {
 	int i, j;
-	
 	for (i = '0'; i < '9'; i++)
 	{
-	
 	for (j = i + 1; j <= '9'; j++)
 	{
 	if (j != i)
-	{	
+	{
 	putchar(i);
 	putchar(j);
 	if (i == '8' && j == '9')
