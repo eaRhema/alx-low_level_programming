d800333c5fd35a219bec7572d237e3ca8571919b
emmanuel addy kofi okrah <emmanueladdy16.ea@gmail.com>
2023-06-20 23:13:47 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,19 @@
 int main(void)
 {
 	int i = 0;
-	long j = l, k = 2;
+	long j = 1, k = 2;
 
 	while (i < 50)
 	{
 	if (i == 0)
 	printf("%ld", j);
 	else if (i == 1)
-	printf(", %d", k);
+	printf(", %ld", k);
 	else
 	{
 	k += j;
 	j = k - j;
-	printf("\n");
+	printf("%ld");
 	}
 	++i;
 	}
