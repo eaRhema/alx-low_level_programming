4af67b3d3c2b107888d166f7f03a138b98f58865
emmanuel addy kofi okrah <emmanueladdy16.ea@gmail.com>
2023-06-20 22:15:22 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * add - adds two values 
+ * add - adds two values
  * @n1: the first number to be computed
  * @n2: the second number to be computed
  * Return: Integer Value
