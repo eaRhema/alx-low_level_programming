e473286f9eeec127b567db3d31d22f27976568db
emmanuel addy kofi okrah <emmanueladdy16.ea@gmail.com>
2023-06-20 19:50:00 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _abs - Computes the absolute value
+ * of an integer
+ *
+ * @i: input number as an integer
+ *
+ * Return: Aabsolute value
+ */
+
+int _abs(int 1)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
